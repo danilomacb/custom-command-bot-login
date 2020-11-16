bfa5d52b882e1a4decc2aa590c9e34e64cd747c3
danilomacb <danilo.bakun@gmail.com>
2020-11-16 07:43:45 -0300

    Discord server schema

---
+++ backend/models/DiscordServer.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DiscordServerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  discordServerId: { type: String, required: true },
+  discordUsers: { type: mongoose.Schema.Types.ObjectId, ref: User },
+  discordAdmins: { type: mongoose.Schema.Types.ObjectId, ref: User },
+});
+
+module.exports = mongoose.model("DiscordServer", DiscordServerSchema);
